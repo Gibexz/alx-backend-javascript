1ceb2f7b1f0e308fe5813dab550555fd653a4364
Gibexz <andrewetedu@gmail.com>
2023-10-04 00:27:42 +0100

    8. Clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,11 +1,24 @@
+// export default function cleanSet(set, startString) {
+//   const result = [];
+
+//   for (const value of set) {
+//     if (value.startsWith(startString) && startString.length > 0) {
+//       result.push(value.slice(startString.length));
+//     }
+//   }
+
+//   return result.join('-');
+// }
 export default function cleanSet(set, startString) {
-  const result = [];
-
-  for (const value of set) {
-    if (value.startsWith(startString) && startString.length > 0) {
-      result.push(value.slice(startString.length));
+  if (!(set instanceof Set) || typeof startString !== 'string' || !set || !startString) return '';
+  const matches = [];
+  set.forEach((key) => {
+    if (typeof key === 'string' && key.startsWith(startString) && startString.length > 0) {
+      // substring() removes the startString and appends the rest
+      matches.push(key.substring(startString.length));
     }
-  }
+  });
 
-  return result.join('-');
+  const result = matches.join('-');
+  return result;
 }
