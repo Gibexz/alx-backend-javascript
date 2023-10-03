a690d7ed881fffed9a5a82cbb984c41d1dd9de25
Gibexz <andrewetedu@gmail.com>
2023-10-04 00:24:48 +0100

    8. Clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,8 @@
 export default function cleanSet(set, startString) {
   const result = [];
-  if (startString === '') return '';
 
   for (const value of set) {
-    if (value.startsWith(startString)) {
+    if (value.startsWith(startString) && startString.length > 0) {
       result.push(value.slice(startString.length));
     }
   }
