ea7d1f82330c7628ebe6378d5717710eeab2e328
Gibexz <andrewetedu@gmail.com>
2023-11-23 22:29:22 +0100

    Task 2. Basic test using Chai assertion library v3

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,5 +1,5 @@
 const { expect } = require('chai');
-const calculateNumber = require('./1-calcul');
+const calculateNumber = require('./2-calcul_chai');
 
 describe('tests for calculateNumber function - All returned values are rounded', () => {
   describe('type === "SUM"', () => {
